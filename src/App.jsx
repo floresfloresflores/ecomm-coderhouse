ccf9d9f2954973a9659a0f5e65c5d2d93a7a5ba4
Ignacio Flores <Ignacio.Flores@gds.ey.com>
2024-04-22 17:33:27 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import {Navbar} from '.component/Navbar.jsx'
+
+function App() {
+    const title = "Welcome!"
+    return (
+      <>
+        <Navbar />
+      </>
+    )
+}
+
+export default App
